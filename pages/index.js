a9708a50b5e060d72776dd2dc9d4a59ea22ba61d
James Singleton <jamesrsingleton1@gmail.com>
2020-10-26 19:47:34 -0700

    chore(title): update index.js title

--- pages/index.js
+++ pages/index.js
@@ -5,7 +5,7 @@ import HeroPost from '../components/hero-post'
 import Intro from '../components/intro'
 import Layout from '../components/layout'
 import { getAllPostsForHome } from '../lib/api'
-import { CMS_NAME } from '../lib/constants'
+import { NAME } from '../lib/constants'
 
 export default function Index({ allPosts: { edges }, preview }) {
   const heroPost = edges[0]?.node
@@ -15,7 +15,7 @@ export default function Index({ allPosts: { edges }, preview }) {
     <>
       <Layout preview={preview}>
         <Head>
-          <title>Next.js Blog Example with {CMS_NAME}</title>
+          <title>Home - {NAME}</title>
           <script src="https://unpkg.com/ionicons@5.2.3/dist/ionicons.js"></script>
         </Head>
         <Container className="text-secondary bg-secondary">
