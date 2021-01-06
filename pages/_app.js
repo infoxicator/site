1d19d176d4aab8b3ab7b929c9428f0774489ac72
infoxicator <ruben@infoxication.net>
2021-01-06 23:55:57 +0000

    feat(i18n): initial implementation

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,21 @@
 import '../styles/index.css'
 import { ThemeProvider } from 'use-theme-switcher';
+import { IntlProvider } from 'react-intl';
+import { useRouter } from "next/router"
+
+const languages = {
+  en: require('../locale/en.json'),
+  es: require('../locale/es.json')
+};
 
 function MyApp({ Component, pageProps }) {
+  const router = useRouter()
+  const { locale, defaultLocale } = router;
+  const messages = languages[locale];
   return <ThemeProvider>
+     <IntlProvider messages={messages} locale={locale} defaultLocale={defaultLocale}>
       <Component {...pageProps} />
+      </IntlProvider>
     </ThemeProvider>
 }
 
